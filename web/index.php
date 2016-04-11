21aa202928539d21418cdbaa7ae9234532f7bd0d
Eddie Wong <isp396@gmail.com>
2016-04-11 14:35:15 +0800

    Edited index.php

--- web/index.php
+++ web/index.php
@@ -1,11 +1,11 @@
 <?php
 
-    require('../vendor/autoload.php');
+    // require('../vendor/autoload.php');
     // load up your config file
     require_once("/resources/config.php");
      
-    include_once("/views/header.html");
-    include_once("/views/nav.html");
+    require_once(TEMPLATES_PATH . "/views/header.html");
+    require_once(TEMPLATES_PATH . "/views/nav.html");
 ?>
 <div id="container">
     <div id="content">
